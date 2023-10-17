751231125f3e3c5850fa5193ef61c89168e112b5
Shrey <shreyshukla1010@gmail.com>
2023-10-17 19:36:48 +0530

    level 1 relationship completed

---
+++ myproject/myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    teachers = models.ManyToManyField(Teacher)
